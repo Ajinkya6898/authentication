bf767f9e2e86abba7f49495d1a6ed0cb9a8f8d8b
Ajinkya Joshi <p7167185@ness.com>
2024-02-25 08:24:27 +0530

    add ui for login and register pages

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "../styles/form.css";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className="container">
+      <div className="wrapper">
+        <form>
+          <h1>Login</h1>
+          <input type="text" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <div className="forgot-password">
+            <Link>Forgot Password?</Link>
+          </div>
+          <button className="btn">Login</button>
+          <div>
+            <p>
+              Not a member! <Link to={"/register"}>Register Now</Link>
+            </p>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import "../styles/form.css";
 
 const Register = () => {
   return (
-    <div>Register</div>
-  )
-}
+    <div className="container">
+      <div className="wrapper">
+        <form>
+          <h1>Register</h1>
+          <input type="text" placeholder="Email" />
+          <input type="text" placeholder="Name" />
+          <input type="password" placeholder="Password" />
+          <input type="password" placeholder="Re-Enter Password" />
+          <div className="terms">
+            <input type="checkbox" />
+            <label htmlFor="checkbox">
+              I agree to these <Link>Terms & Conditions</Link>
+            </label>
+          </div>
+          <button className="btn">Login</button>
+          <div>
+            <p>
+              Already a member <Link to={"/login"}>Login Here!</Link>
+            </p>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
 
-export default Register
+export default Register;
